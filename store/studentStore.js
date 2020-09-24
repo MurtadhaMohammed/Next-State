9f70b8d213d3a127afef21506fb4b2a7a7bf3706
MurtadhaMohammed <mumu9943@gmail.com>
2020-09-24 19:46:59 +0300

    first commit

---
+++ store/studentStore.js
@@ -0,0 +1,18 @@
+import create from "zustand";
+
+const StudentStore = create((set) => ({
+  name: null,
+  age: null,
+  address: null,
+  gender: null,
+  data: [],
+  isModal: false,
+  setIsModal: (value) => set({ isModal: value }),
+  setData: (value) => set({ data: value }),
+  setName: (value) => set({ name: value }),
+  setAge: (value) => set({ age: value }),
+  setAddress: (value) => set({ address: value }),
+  setGender: (value) => set({ gender: value }),
+}));
+
+export default StudentStore;
